fdf17c7b2969d55e70ce2a7956828646c3db6207
caique325 <caique-magalhaes1@outlook.com>
2023-10-10 18:04:56 -0300

    testando o projeto

--- js/main.js
+++ js/main.js
@@ -1,3 +1,36 @@
-var titulo = document.querySelector(".titulo")
+var titulo = document.querySelector(".titulo");
+titulo.textContent = "Aparecida Nutricionista";
 
-titulo.textContent = "Aparecida Nutricionista"
+var paciente = document.querySelector("#primeiro-paciente");
+var tdPeso =paciente.querySelector(".info-peso");
+
+var tdAltura = paciente.querySelector(".info-altura");
+var altura = tdAltura.textContent;
+var peso = tdPeso.textContent;
+
+
+var tdImc = paciente.querySelector(".info-imc")
+
+
+var pesoEhValido = true;
+var alturaEhvalida = true ;
+
+if(peso <= 0 || peso >= 1000) {
+    console.log("peso Invalido");
+    pesoEhValido = false;
+    tdImc.textContent= "Peso invalido"
+}
+
+if(altura <= 0 || altura > 3) {
+    console.log("Altura Invalida");
+    alturaEhvalida = false; 
+    tdImc.textContent= "Altura Invalida"
+
+}
+
+if(alturaEhvalida && pesoEhValido) {
+    var imc = peso / altura **2;
+    var imc_tabela = paciente.querySelector(".info-imc").textContent = imc;
+
+
+}
