9e6f6418f8130c8fc02044d86572811206caeb7a
caique325 <caique-magalhaes1@outlook.com>
2023-10-12 16:05:22 -0300

    add outros pacientes

--- js/main.js
+++ js/main.js
@@ -42,3 +42,52 @@ for(var i =0; i < pacientes.length; i++) {
         
     }
 }
+
+/*titulo.addEventListener("click", function (){
+    console.log("ola mundo")
+});// funcao anonima*/
+
+/*function mostraMnsg() {
+    console.log("Ola mundo")
+}
+*/
+//botaoAdd.addEventListener('click', mostraMnsg) 
+
+var botaoAdd = document.querySelector("#adicionar-paciente")
+
+botaoAdd.addEventListener("click",function(){
+    event.preventDefault();
+
+    var form = document.querySelector("#form-adiciona")
+    
+    var nome = form.nome.value;
+    var altura = form.altura.value;
+    var peso = form.peso.value;
+    var gordura = form.gordura.value;
+
+   var pacienteTr= document.createElement("tr");
+   
+   var nomeTd = document.createElement("td")
+   var pesoTd = document.createElement("td")
+   var alturaTd = document.createElement("td")
+   var gorduraTd = document.createElement("td")
+   var imcTd = document.createElement("td")
+
+   nomeTd.textContent = nome;
+   pesoTd.textContent = peso;
+   alturaTd.textContent = altura;
+   gorduraTd.textContent = gordura;
+   imcTd.textContent = imc;
+
+   pacienteTr.appendChild(nomeTd)
+   pacienteTr.appendChild(pesoTd)
+   pacienteTr.appendChild(alturaTd)
+   pacienteTr.appendChild(gorduraTd)
+   pacienteTr.appendChild(imcTd)
+
+   var tabela = document.querySelector('#tabela-pacientes')
+   tabela.appendChild(pacienteTr)
+
+    
+})
+
