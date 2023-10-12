dd3f1555f6666fe4008b7479938866f3ec94f999
caique325 <caique-magalhaes1@outlook.com>
2023-10-11 23:32:25 -0300

    atualizando main.js e indes.css

--- js/main.js
+++ js/main.js
@@ -1,36 +1,44 @@
 var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista";
 
-var paciente = document.querySelector("#primeiro-paciente");
-var tdPeso =paciente.querySelector(".info-peso");
+var pacientes = document.querySelectorAll(".paciente");
 
-var tdAltura = paciente.querySelector(".info-altura");
-var altura = tdAltura.textContent;
-var peso = tdPeso.textContent;
+for(var i =0; i < pacientes.length; i++) {
+    var paciente = pacientes[i];
+
+    var tdPeso =paciente.querySelector(".info-peso");
+    var tdAltura = paciente.querySelector(".info-altura");
+    var altura = tdAltura.textContent;
+    var peso = tdPeso.textContent;
 
 
-var tdImc = paciente.querySelector(".info-imc")
+    var tdImc = paciente.querySelector(".info-imc")
 
 
-var pesoEhValido = true;
-var alturaEhvalida = true ;
+    var pesoEhValido = true;
+    var alturaEhvalida = true ;
 
-if(peso <= 0 || peso >= 1000) {
-    console.log("peso Invalido");
-    pesoEhValido = false;
-    tdPeso.textContent= "Peso invalido"
-}
+    if(peso <= 0 || peso >= 1000) {
+        console.log("peso Invalido");
+        pesoEhValido = false;
+        tdPeso.textContent= "Peso invalido"
+    }
 
-if(altura <= 0 || altura > 3) {
-    console.log("Altura Invalida");
-    alturaEhvalida = false; 
-    tdAltura.textContent= "Altura Invalida"
+    if(altura <= 0 || altura > 3) {
+        console.log("Altura Invalida");
+        alturaEhvalida = false; 
+        tdAltura.textContent= "Altura Invalida"
 
-}
+    }
 
-if(alturaEhvalida && pesoEhValido) {
-    var imc = peso / altura **2;
-    var imc_tabela = paciente.querySelector(".info-imc").textContent = imc;
-}else{
-    tdImc.textContent = 'Altura ou peso invalido';
+    if(alturaEhvalida && pesoEhValido) {
+        var imc = peso / altura **2;
+        var imc_tabela = paciente.querySelector(".info-imc").textContent = imc.toFixed(2);
+        
+    }else{
+        paciente.classList.add("paciente-invalido");
+        //paciente.style.backgroundColor = "red"
+        tdImc.textContent = 'Altura ou peso invalido';
+        
+    }
 }
