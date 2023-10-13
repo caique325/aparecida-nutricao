5f6240f1322c1f35e61962cc8dd2162695c0aca9
caique325 <caique-magalhaes1@outlook.com>
2023-10-13 19:06:01 -0300

    dividindo em arquivos e funcoes

---
+++ js/calc-imc.js
@@ -0,0 +1,61 @@
+var titulo = document.querySelector(".titulo");
+titulo.textContent = "Aparecida Nutricionista";
+
+var pacientes = document.querySelectorAll(".paciente");
+
+for(var i =0; i < pacientes.length; i++) {
+    var paciente = pacientes[i];
+
+    var tdPeso =paciente.querySelector(".info-peso");
+    var tdAltura = paciente.querySelector(".info-altura");
+    var altura = tdAltura.textContent;
+    var peso = tdPeso.textContent;
+
+
+    var tdImc = paciente.querySelector(".info-imc")
+
+
+    var pesoEhValido = true;
+    var alturaEhvalida = true ;
+
+    if(peso <= 0 || peso >= 1000) {
+        console.log("peso Invalido");
+        pesoEhValido = false;
+        tdPeso.textContent= "Peso invalido"
+    }
+
+    if(altura <= 0 || altura > 3) {
+        console.log("Altura Invalida");
+        alturaEhvalida = false; 
+        tdAltura.textContent= "Altura Invalida"
+
+    }
+
+    if(alturaEhvalida && pesoEhValido) {
+        var imc = calculaImc(peso,altura);
+        var imc_tabela = paciente.querySelector(".info-imc").textContent = imc
+        
+    }else{
+        paciente.classList.add("paciente-invalido");
+        //paciente.style.backgroundColor = "red"
+        tdImc.textContent = 'Altura ou peso invalido';
+        
+    }
+}
+
+
+function calculaImc(peso,altura){
+    var imc = 0;
+    var imc = peso / altura**2;
+    return imc.toFixed(2);
+}
+/*titulo.addEventListener("click", function (){
+    console.log("ola mundo")
+});// funcao anonima*/
+
+/*function mostraMnsg() {
+    console.log("Ola mundo")
+}
+*/
+//botaoAdd.addEventListener('click', mostraMnsg) 
+
